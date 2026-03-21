e53206cc04c47e13923e0dfe5a11d5bee1782dd7
himanshi <pariharhimanshi02@gmail.com>
2026-03-21 22:32:27 +0530

    new fix

--- src/landing_page/products/LeftSection.js
+++ src/landing_page/products/LeftSection.js
@@ -13,7 +13,7 @@ function LeftSection({
     <div className="container mt-5">
       <div className="row ">
         <div className="col-6">
-          <img src={imageURL} />
+          <img src={imageURL} alt="" />
         </div>
         <div className="col-6 p-5 mt-5">
           <h1>{productName}</h1>
@@ -26,11 +26,11 @@ function LeftSection({
           </div>
           <div className="mt-3">
             <a href={googlePlay}>
-              <img src="media/images/googlePlayBadge.svg" />
+              <img src="media/images/googlePlayBadge.svg"  alt=""/>
             </a>
             <a href={appStore}>
               <img
-                src="media/images/appStoreBadge.svg"
+                src="media/images/appStoreBadge.svg" alt=""
                 style={{ marginLeft: "50px" }}
               />
             </a>
--- src/landing_page/products/ProductPage.js
+++ src/landing_page/products/ProductPage.js
@@ -20,13 +20,13 @@ function ProductPage() {
         appStore=""
       />
       <RightSection
-        imageURL="media/images/console.png"
+        imageURL="media/images/console.png" alt=""
         productName="Console"
         productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
         learnMore=""
       />
       <LeftSection
-        imageURL="media/images/coin.png"
+        imageURL="media/images/coin.png" alt=""
         productName="Coin"
         productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
         tryDemo=""
@@ -35,13 +35,13 @@ function ProductPage() {
         appStore=""
       />
       <RightSection
-        imageURL="media/images/kiteconnect.png"
+        imageURL="media/images/kiteconnect.png" alt=""
         productName="Kite Connect API"
         productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
         learnMore=""
       />
       <LeftSection
-        imageURL="media/images/varsity.png"
+        imageURL="media/images/varsity.png" alt=""
         productName="Varsity mobile"
         productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
         tryDemo=""
