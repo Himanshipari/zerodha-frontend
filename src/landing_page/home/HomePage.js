56d976bd33ca08e6784bb1b2ff173abf55c82fa7
himanshi <pariharhimanshi02@gmail.com>
2026-03-21 17:27:43 +0530

    frontend

--- src/landing_page/home/HomePage.js
+++ src/landing_page/home/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from './Hero';
 import Awards from './Awards';
 import Stats from './Stats';
-import Pricing from './Pricing.js';
+import Pricing from './pricing.js';
 import Education from './Education';
 import OpenAccount from '../OpenAccount';
 import Navbar from '../Navbar';
