9988ac56af811c8d1a1b4c17a7081cbff17b9780
himanshi <pariharhimanshi02@gmail.com>
2026-03-21 18:00:15 +0530

    frontend

--- src/landing_page/support/SupportPage.js
+++ src/landing_page/support/SupportPage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Hero from './Hero';
 import CreateSupport from './CreateSupport';
-// import Navbar from '../Navbar';
-// import Footer from '../Footer';
 
 function SupportPage() {
     return (
