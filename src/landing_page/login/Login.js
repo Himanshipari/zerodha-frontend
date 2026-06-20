bd3cfac3e4b303d52d6b2dc8fc44600a9a364994
himanshi <pariharhimanshi02@gmail.com>
2026-06-20 23:36:03 +0530

    fixed exact backend login url

--- src/landing_page/login/Login.js
+++ src/landing_page/login/Login.js
@@ -146,6 +146,154 @@
 
 
 
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+// import { ToastContainer, toast } from "react-toastify";
+
+// const Login = () => {
+
+//   const toDashboard = () => {
+//     window.location.href = "https://zerodha-dashboard-6gww.onrender.com";
+//   };
+
+//   const [inputValue, setInputValue] = useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const { email, password } = inputValue;
+
+//   const handleOnChange = (e) => {
+//     const { name, value } = e.target;
+
+//     setInputValue({
+//       ...inputValue,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleError = (err) =>
+//     toast.error(err, {
+//       position: "bottom-left",
+//     });
+
+//   const handleSuccess = (msg) =>
+//     toast.success(msg, {
+//       position: "bottom-left",
+//     });
+
+//   const handleSubmit = async (e) => {
+
+//     e.preventDefault();
+
+//     try {
+
+//       // यहाँ पर आपका लाइव बैकेंड लिंक सफलतापूर्वक सेट कर दिया गया है
+//       const { data } = await axios.post(
+//         "https://onrender.com",
+//         {
+//           email,
+//           password,
+//         },
+//         { withCredentials: true }
+//       );
+
+//       const { success, message } = data;
+
+//       if (success) {
+
+//         handleSuccess(message);
+
+//         setTimeout(() => {
+//           toDashboard();
+//         }, 1000);
+
+//       } else {
+//         handleError(message);
+//       }
+
+//     } catch (error) {
+//       console.log(error);
+//       handleError("Login failed");
+//     }
+
+//     setInputValue({
+//       email: "",
+//       password: "",
+//     });
+//   };
+
+//   return (
+
+//     <div className="container p-3 p-sm-5 text-center">
+
+//       <form onSubmit={handleSubmit}>
+
+//         <div className="row">
+
+//           <div className="col-12 col-sm-7">
+//             <img
+//               src="/media/images/signup.png"
+//               alt="login"
+//               style={{ width: "100%" }}
+//             />
+//           </div>
+
+//           <div className="col-12 col-sm-5 p-3 p-sm-5">
+
+//             <h2 className="text-center">Login</h2>
+
+//             <div className="mt-4">
+
+//               <input
+//                 className="form-control mb-3"
+//                 type="email"
+//                 name="email"
+//                 value={email}
+//                 placeholder="Enter your email"
+//                 onChange={handleOnChange}
+//                 required
+//               />
+
+//               <input
+//                 className="form-control mb-3"
+//                 type="password"
+//                 name="password"
+//                 value={password}
+//                 placeholder="Enter your password"
+//                 onChange={handleOnChange}
+//                 required
+//               />
+
+//               <button
+//                 type="submit"
+//                 className="btn btn-primary w-100"
+//               >
+//                 Submit
+//               </button>
+
+//               <p className="mt-3">
+//                 Don't have an account? <Link to="/signup">Signup</Link>
+//               </p>
+
+//             </div>
+
+//           </div>
+
+//         </div>
+
+//       </form>
+
+//       <ToastContainer />
+
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -154,7 +302,7 @@ import { ToastContainer, toast } from "react-toastify";
 const Login = () => {
 
   const toDashboard = () => {
-    window.location.href = "https://zerodha-dashboard-6gww.onrender.com";
+    window.location.href = "https://onrender.com";
   };
 
   const [inputValue, setInputValue] = useState({
@@ -184,12 +332,10 @@ const Login = () => {
     });
 
   const handleSubmit = async (e) => {
-
     e.preventDefault();
 
     try {
-
-      // यहाँ पर आपका लाइव बैकेंड लिंक सफलतापूर्वक सेट कर दिया गया है
+      // यहाँ पर आपका असली और सही लाइव बैकेंड लिंक सेट कर दिया गया है
       const { data } = await axios.post(
         "https://onrender.com",
         {
@@ -202,13 +348,10 @@ const Login = () => {
       const { success, message } = data;
 
       if (success) {
-
         handleSuccess(message);
-
         setTimeout(() => {
           toDashboard();
         }, 1000);
-
       } else {
         handleError(message);
       }
@@ -225,13 +368,9 @@ const Login = () => {
   };
 
   return (
-
     <div className="container p-3 p-sm-5 text-center">
-
       <form onSubmit={handleSubmit}>
-
         <div className="row">
-
           <div className="col-12 col-sm-7">
             <img
               src="/media/images/signup.png"
@@ -239,13 +378,9 @@ const Login = () => {
               style={{ width: "100%" }}
             />
           </div>
-
           <div className="col-12 col-sm-5 p-3 p-sm-5">
-
             <h2 className="text-center">Login</h2>
-
             <div className="mt-4">
-
               <input
                 className="form-control mb-3"
                 type="email"
@@ -255,7 +390,6 @@ const Login = () => {
                 onChange={handleOnChange}
                 required
               />
-
               <input
                 className="form-control mb-3"
                 type="password"
@@ -265,28 +399,17 @@ const Login = () => {
                 onChange={handleOnChange}
                 required
               />
-
-              <button
-                type="submit"
-                className="btn btn-primary w-100"
-              >
+              <button type="submit" className="btn btn-primary w-100">
                 Submit
               </button>
-
               <p className="mt-3">
                 Don't have an account? <Link to="/signup">Signup</Link>
               </p>
-
             </div>
-
           </div>
-
         </div>
-
       </form>
-
       <ToastContainer />
-
     </div>
   );
 };
