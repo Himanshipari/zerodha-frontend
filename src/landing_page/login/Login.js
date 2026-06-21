c3c4a2b8a50c52cdc563b0678de202322d819078
himanshi <pariharhimanshi02@gmail.com>
2026-06-21 22:58:43 +0530

    Fix frontend index file

--- src/landing_page/login/Login.js
+++ src/landing_page/login/Login.js
@@ -398,13 +398,117 @@
 
 // export default Login;
 
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+// import { ToastContainer, toast } from "react-toastify";
+
+// const Login = () => {
+
+//   const toDashboard = () => {
+//     window.location.href = "https://zerodha-dashboard-6gww.onrender.com";
+//   };
+
+//   const [inputValue, setInputValue] = useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const { email, password } = inputValue;
+
+//   const handleOnChange = (e) => {
+//     const { name, value } = e.target;
+//     setInputValue({
+//       ...inputValue,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleError = (err) =>
+//     toast.error(err, { position: "bottom-left" });
+
+//   const handleSuccess = (msg) =>
+//     toast.success(msg, { position: "bottom-left" });
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       // 🛠️ बदलाव यहाँ किया गया है: URL के आखिर में /login जोड़ा गया है और 'nu2g' किया गया है जैसा इमेज में था
+//       const { data } = await axios.post(
+//         "https://onrender.com",
+//         { email, password },
+//         { withCredentials: true }
+//       );
+
+//       const { success, message } = data;
+
+//       if (success) {
+//         handleSuccess(message);
+//         setTimeout(() => {
+//           toDashboard();
+//         }, 1000);
+//       } else {
+//         handleError(message);
+//       }
+
+//     } catch (error) {
+//       console.log(error);
+//       handleError("Login failed");
+//     }
+
+//     setInputValue({ email: "", password: "" });
+//   };
+
+//   return (
+//     <div className="container p-3 p-sm-5 text-center">
+//       <form onSubmit={handleSubmit}>
+//         <div className="row">
+//           <div className="col-12 col-sm-7">
+//             <img src="/media/images/signup.png" alt="login" style={{ width: "100%" }} />
+//           </div>
+//           <div className="col-12 col-sm-5 p-3 p-sm-5">
+//             <h2 className="text-center">Login</h2>
+//             <div className="mt-4">
+//               <input
+//                 className="form-control mb-3"
+//                 type="email"
+//                 name="email"
+//                 value={email}
+//                 placeholder="Enter your email"
+//                 onChange={handleOnChange}
+//                 required
+//               />
+//               <input
+//                 className="form-control mb-3"
+//                 type="password"
+//                 name="password"
+//                 value={password}
+//                 placeholder="Enter your password"
+//                 onChange={handleOnChange}
+//                 required
+//               />
+//               <button type="submit" className="btn btn-primary w-100">Submit</button>
+//               <p className="mt-3">
+//                 Don't have an account? <Link to="/signup">Signup</Link>
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </form>
+//       <ToastContainer />
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 
 const Login = () => {
-
   const toDashboard = () => {
     window.location.href = "https://zerodha-dashboard-6gww.onrender.com";
   };
@@ -434,9 +538,8 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      // 🛠️ बदलाव यहाँ किया गया है: URL के आखिर में /login जोड़ा गया है और 'nu2g' किया गया है जैसा इमेज में था
       const { data } = await axios.post(
-        "https://onrender.com",
+        "https://zerodha-backend-nu2q.onrender.com/login",
         { email, password },
         { withCredentials: true }
       );
@@ -445,19 +548,22 @@ const Login = () => {
 
       if (success) {
         handleSuccess(message);
+
         setTimeout(() => {
           toDashboard();
         }, 1000);
       } else {
         handleError(message);
       }
-
     } catch (error) {
       console.log(error);
       handleError("Login failed");
     }
 
-    setInputValue({ email: "", password: "" });
+    setInputValue({
+      email: "",
+      password: "",
+    });
   };
 
   return (
@@ -465,10 +571,16 @@ const Login = () => {
       <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-12 col-sm-7">
-            <img src="/media/images/signup.png" alt="login" style={{ width: "100%" }} />
+            <img
+              src="/media/images/signup.png"
+              alt="login"
+              style={{ width: "100%" }}
+            />
           </div>
+
           <div className="col-12 col-sm-5 p-3 p-sm-5">
             <h2 className="text-center">Login</h2>
+
             <div className="mt-4">
               <input
                 className="form-control mb-3"
@@ -479,6 +591,7 @@ const Login = () => {
                 onChange={handleOnChange}
                 required
               />
+
               <input
                 className="form-control mb-3"
                 type="password"
@@ -488,7 +601,11 @@ const Login = () => {
                 onChange={handleOnChange}
                 required
               />
-              <button type="submit" className="btn btn-primary w-100">Submit</button>
+
+              <button type="submit" className="btn btn-primary w-100">
+                Submit
+              </button>
+
               <p className="mt-3">
                 Don't have an account? <Link to="/signup">Signup</Link>
               </p>
@@ -496,10 +613,10 @@ const Login = () => {
           </div>
         </div>
       </form>
+
       <ToastContainer />
     </div>
   );
 };
 
 export default Login;
-
